ea6fcb3e1ee2b370170fac5f44fb247e5e16a71b
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2025-01-02 14:30:31 +0530

    commit 4

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ const HomePage = () => (
       <div className="flex items-center justify-center">
         <div
           className="text-center my-10 border-2 border-white rounded-[30px] 
-                 shadow-lg px-8 py-8 sm:p-14 md:p-16 lg:p-20 xl:p-10"
+                 shadow-lg px-8 py-8"
           style={{
             boxShadow: "0 10px 15px -3px #a5b4fc, 0 4px 6px -4px rgb(0 69 76)",
+            width: "100vh"
           }}
         >
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-0 
+          <h1 className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold mb-0 
                      animate-[gradient_6s_linear_infinite] 
                      bg-gradient-to-r from-gray-200 via-indigo-200 to-gray-50 
                      bg-[length:200%_auto] bg-clip-text 
